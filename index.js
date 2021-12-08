c1ad9773d5d3d79d71e232fe995d0ec16ce0a1f5
Alexander Tambovsky <tambovskya@gmail.com>
2021-12-08 00:14:15 -0500

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-	rese.send("Working!");
+	res.send("Working!");
 });
 
 app.listen(process.env.PORT || 5000);
