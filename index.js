ff298c60c213131e0d2147010105d5d5d9f79eaf
Alexander Tambovsky <tambovskya@gmail.com>
2021-12-08 00:15:58 -0500

    updates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 app.get("/", function(req, res) {
 	res.send("Working!");
