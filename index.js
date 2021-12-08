194ab50b90ebe792f05764a4bba8169d4e8ad5d1
Alexander Tambovsky <tambovskya@gmail.com>
2021-12-08 00:28:03 -0500

    updates

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const socket = require("socket.io");
+
+const PORT = 5000;
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+
+const server = app.listen(PORT, function() {
+	console.log("Hello Good Friend!");
+});
+
+const io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+});
