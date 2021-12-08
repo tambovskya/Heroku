c8db2da879f4f0c030bb832a8a24f238e21a0968
Alexander Tambovsky <tambovskya@gmail.com>
2021-12-08 00:40:44 -0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-
-server.listen(process.env.PORT || 3000);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
